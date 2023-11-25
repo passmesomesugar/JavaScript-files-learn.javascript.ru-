7d36877e8b1f936e6965c5239f39ca03d7471e06
Sadvakassov Bagzhan <sadvakasov.bagjan@gmail.com>
2023-11-25 11:18:42 +0600

    progress

---
+++ fundamentals/2.11 logical operators/or.js
@@ -0,0 +1,13 @@
+console.log(true || true);   // true
+console.log(false || true);  // true
+console.log(true || false);  // true
+console.log(false || false); // false
+
+
+console.log("This returns first true value or last value");
+console.log(null || 1); // 1 first true value
+console.log(null || 0 || 1); // 1 first true value
+console.log(undefined || null || 0); // 0 all is false thus last is returned
+
+true || console.log("never executed"); // stops at true, has no chance reaching second statement
+false || console.log("executed");  
